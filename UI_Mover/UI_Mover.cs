b9689a129863b82fc5c0639b96bdd424eb6a9f39
Krutonium <PFCKrutonium@gmail.com>
2020-04-30 02:44:47 -0400

    Fix breaking if FPS display is off, and update version to 1.1

--- UI_Mover/UI_Mover.cs
+++ UI_Mover/UI_Mover.cs
@@ -12,7 +12,7 @@ namespace UI_Mover
         public override string ID => "UI_Mover"; //Your mod ID (unique)
         public override string Name => "UI Mover"; //You mod name
         public override string Author => "Krutonium"; //Your Username
-        public override string Version => "1.0"; //Version
+        public override string Version => "1.1"; //Version
 
         // Set this to true if you will be load custom assets from Assets folder.
         // This will create subfolder in Assets folder for your mod.
@@ -35,6 +35,7 @@ namespace UI_Mover
             public static Vector3 Money;
             public static Vector3 FPS;
         }
+        static bool fps = true;
 
         public override void OnLoad()
         {
@@ -53,7 +54,14 @@ namespace UI_Mover
             originals.Urine = GameObject.Find("Urine").transform.position;
             originals.Dirty = GameObject.Find("Dirty").transform.position;
             originals.Money = GameObject.Find("Money").transform.position;
-            originals.FPS = GameObject.Find("FPS").transform.position;
+            try
+            {
+                originals.FPS = GameObject.Find("FPS").transform.position;
+            } catch (Exception ex)
+            {
+                fps = false;
+            }
+            
         }
         private static void RestoreInitialValues()
         {
@@ -65,7 +73,10 @@ namespace UI_Mover
             GameObject.Find("Urine").transform.position = originals.Urine;
             GameObject.Find("Dirty").transform.position = originals.Dirty;
             GameObject.Find("Money").transform.position = originals.Money;
-            GameObject.Find("FPS").transform.position = originals.FPS;
+            if (fps)
+            {
+                GameObject.Find("FPS").transform.position = originals.FPS;
+            }
         }
 
         private static void Move(string thingToMove, bool inverted = false, int distance = 5) //5 is ideal for 16:9
@@ -108,7 +119,10 @@ namespace UI_Mover
             Move("Urine", false, V);
             Move("Dirty", false, V);
             Move("Money", false, V);
-            Move("FPS", true, V);
+            if (fps)
+            {
+                Move("FPS", true, V);
+            }
         }
         public override void OnSave()
         {
